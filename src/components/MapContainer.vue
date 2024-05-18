fd57cbcf910168c1b296f99c22f116389e8dc974
bytewiz3 <46955122+bytewiz3@users.noreply.github.com>
2024-05-17 21:01:16 -0700

    add watcher for mapMsg changes to handle marker updates

--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -6,4 +6,34 @@
 import { inject, onMounted, onUnmounted, watch } from "vue";
 import AMapLoader from "@amap/amap-jsapi-loader";
 
+et map = null;
+const mapMsg = inject("handleMapMsg", null);
+const icons = [
+  {
+    type: 1,
+    icon: "🏨", // hotel
+  },
+  {
+    type: 2,
+    icon: "🍽️", // restaurant
+  },
+];
+watch(
+  () => mapMsg.value,
+  (v) => {
+    console.log(v);
+    switch (v?.type) {
+      case 1:
+        setMaket(v);
+        break;
+
+      default:
+        break;
+    }
+  },
+  {
+    deep: true,
+  }
+);
+
 
