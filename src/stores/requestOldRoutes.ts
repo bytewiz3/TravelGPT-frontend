5d855502ea492a7a7ad3d80e211ce4ef176f596d
Yuling <lindowu@foxmail.com>
2024-05-17 23:20:00 -0700

    fix: rename useRequestOriginRoutes to useRequestOldRoutes

--- src/stores/requestOldRoutes.ts
+++ src/stores/requestOldRoutes.ts
@@ -4,13 +4,13 @@ import { defineStore } from 'pinia';
  * 后端返回原始路由(未处理时)
  * @methods setCacheKeepAlive 设置接口原始路由数据
  */
-export const useRequestOriginRoutes = defineStore('RequestOriginRoutes', {
-	state: (): RequestOriginRoutesState => ({
-		RequestOriginRoutes: [],
+export const useRequestOldRoutes = defineStore('requestOldRoutes', {
+	state: (): RequestOldRoutesState => ({
+		requestOldRoutes: [],
 	}),
 	actions: {
-		async setRequestOriginRoutes(routes: Array<string>) {
-			this.RequestOriginRoutes = routes;
+		async setRequestOldRoutes(routes: Array<string>) {
+			this.requestOldRoutes = routes;
 		},
 	},
 });
