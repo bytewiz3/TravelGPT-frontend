d1af7f8443fa25a9ad429bb6e0a664ac14fa01f5
bytewiz3 <46955122+bytewiz3@users.noreply.github.com>
2024-05-19 20:42:41 -0700

    refactor template structure for better readability and flexibility

---
+++ src/components/TextMessage.vue
@@ -0,0 +1,19 @@
+<template>
+	<div
+		class="text-message"
+		:class="{
+			left: float == 'left',
+			right: float == 'right',
+			'max-width': !fullWidth,
+		}"
+	>
+		<div class="btns flex justify-end" v-if="float === 'left'">
+			<div v-for="ite in 4" :key="ite" class="cursor-pointer shake mr-2" @click="handleClick(ite)">
+				<img :src="`./src/views/chat/imgs/icon${ite}.png`" alt="" />
+			</div>
+		</div>
+		<pre v-html="typedText" ref="typedOutput" />
+	</div>
+</template>
+
+
