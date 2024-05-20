909d3e8f99157d37735c057e4cc43317470843a4
limengunique <limengunique0917@gmail.com>
2024-05-19 21:41:35 -0700

    add index.js for UI initiation

---
+++ src/static/js/LinkUE4.js
@@ -0,0 +1,26 @@
+﻿function stop(){ return false; }
+document.oncontextmenu=stop;
+function LinkUE4(){
+	
+	var obj = GetParameterArray((Array.prototype.slice.call(arguments, 0)));
+	alert(obj.functionsName);
+	var test = obj;
+	if (window.ue.ueclass){
+		
+		window.ue.ueclass.linkue4(obj.functionsName);
+	}
+	
+};
+
+function GetParameterArray(LinkUE4Array){
+	var obj = {
+		functionsName:''
+	};
+	obj.functionsName = LinkUE4Array[0];
+	return obj;
+};
+
+function TestBluEvent()
+{
+	window.blu_event();
+}
